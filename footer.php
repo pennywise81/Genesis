9b1230b49fb0aa05cee4abf0f203a53d98c44769
pennywise81 <lars.schweisthal@gmail.com>
2018-07-02 12:29:31 +0200

    General document outline; removed unused scripts and styles

--- footer.php
+++ footer.php
@@ -11,24 +11,28 @@
 
 ?>
 
-	</div><!-- #content -->
+    </div><!-- .site-content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'genesis' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'genesis' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'genesis' ), 'Genesis', '<a href="http://larsschweisthal.de/">Lars Schweisthal</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+    <footer class="site-footer">
+
+      <!--
+      <div class="site-info">
+        <a href="<?php echo esc_url(__('https://wordpress.org/', 'genesis')); ?>">
+          <?php
+          /* translators: %s: CMS name, i.e. WordPress. */
+          printf(esc_html__('Proudly powered by %s', 'genesis'), 'WordPress');
+          ?>
+        </a>
+        <span class="sep"> | </span>
+          <?php
+          /* translators: 1: Theme name, 2: Theme author. */
+          printf(esc_html__('Theme: %1$s by %2$s.', 'genesis'), 'Genesis', '<a href="http://larsschweisthal.de/">Lars Schweisthal</a>');
+          ?>
+      </div>
+      -->
+
+    </footer><!-- .footer -->
+  </div><!-- .site -->
 
 <?php wp_footer(); ?>
 
