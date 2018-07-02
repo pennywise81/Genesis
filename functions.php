e5c2251cec6de74748bbba825a2125c214583db1
pennywise81 <lars.schweisthal@gmail.com>
2018-07-02 12:03:45 +0200

    Added Bootstrap libraries and styles

--- functions.php
+++ functions.php
@@ -120,7 +120,6 @@ add_action( 'widgets_init', 'genesis_widgets_init' );
  * Enqueue scripts and styles.
  */
 function genesis_scripts() {
-	wp_enqueue_style( 'genesis-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'genesis-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
@@ -129,6 +128,12 @@ function genesis_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	wp_enqueue_script('jquery-slim', get_template_directory_uri() . '/js/jquery-3.3.1.slim.min.js', array(), '3.3.1', true);
+	wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.min.js', array(), '1.14.3', true);
+	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array('jquery-slim', 'popper'), '4.1.1', true);
+	wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.1');
+	wp_enqueue_style('genesis-style', get_stylesheet_uri(), array('bootstrap-css'));
 }
 add_action( 'wp_enqueue_scripts', 'genesis_scripts' );
 
