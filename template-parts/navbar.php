932d718fda683cf7465a7a4e7ff6f401e1089f2a
pennywise81 <lars.schweisthal@gmail.com>
2018-07-03 13:46:45 +0200

    Splitted the navbar in more partials

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -22,24 +22,9 @@
 
     <?php
   }
-  else {}
 
   ?>
 
-  <?php
-
-  wp_nav_menu(array(
-    'theme_location' => 'menu-1',
-    'menu_id' => 'primary-menu',
-    'depth' => 2,
-    'container' => 'div',
-    'container_class' => 'collapse navbar-collapse justify-content-end',
-    'container_id' => 'site-primary-menu',
-    'menu_class' => 'nav navbar-nav',
-    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-    'walker' => new WP_Bootstrap_Navwalker(),
-  ));
-
-  ?>
+  <?php get_template_part('template-parts/partials/navbar', 'wp_nav_menu'); ?>
 
 </nav>
---
+++ template-parts/partials/navbar-wp_nav_menu.php
@@ -0,0 +1,13 @@
+  <?php
+
+  wp_nav_menu(array(
+    'theme_location' => 'menu-1',
+    'menu_id' => 'primary-menu',
+    'depth' => 2,
+    'container' => 'div',
+    'container_class' => 'collapse navbar-collapse justify-content-end',
+    'container_id' => 'site-primary-menu',
+    'menu_class' => 'nav navbar-nav',
+    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+    'walker' => new WP_Bootstrap_Navwalker(),
+  ));
